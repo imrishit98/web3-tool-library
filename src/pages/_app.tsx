cc382ad96dcf9c8f383ea9b5bec8fc59c984ac88
jay-esh <anilj@mcmaster.ca>
2022-07-25 12:53:08 +0400

    NavBar changed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
-    </ThemeProvider>
+    </ThemeProvider> 
   );
 }
 
