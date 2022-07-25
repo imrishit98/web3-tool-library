5bc90de71c3f8604c0387068153aa022ab71e847
jay-esh <anilj@mcmaster.ca>
2022-07-25 11:50:50 +0400

    NavBar added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-
+import NavBar from '../../components/NavBar'
 
 const Home: NextPage = () => {
   return (
     <div>
+      
       <Head>
         <title>Web3Pedia</title>
         <meta name="description" content="Hand-picked Tools and Resources for web3 dOers" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <NavBar/>
       <main className='flex w-full h-screen justify-center items-center'>
         <h1 className='text-[100px] font-bold'>
           Web3 Tool Kit
@@ -21,4 +22,4 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Home;
